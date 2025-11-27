7c6eb3d3cb4bf86189591f3fcf773066cc5a7399
jjinjin <hmsoo223@gmail.com>
2025-11-27 15:49:33 +0900

    first commit

--- src/main/java/com/example/empGraphQL/EmpGraphQlApplication.java
+++ src/main/java/com/example/empGraphQL/EmpGraphQlApplication.java
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class EmpGraphQlApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EmpGraphQlApplication.class, args);
+
+        SpringApplication.run(EmpGraphQlApplication.class, args);
+        System.out.println("Hello World!");
 	}
 
 }
